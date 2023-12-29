8557c8e004894c910a85b4c1592e089bd851208b
IvoSa92 <ivo_sauter@web.de>
2023-12-29 16:17:51 +0100

    HTML - created 4 divs for every page view (today,tomorrow..etc) -display:none for activation when the button gets clicked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 console.log("hello");
 
 import DomElements from "./DOM";
-//import { createNewTaskForm } from "./addTask";
-//createNewTaskForm();
 
 import NewTaskManager from "./addTask";
 const taskManager = new NewTaskManager();
