90b577e66ff7975519def7a0b3521a5033456a68
IvoSa92 <ivo_sauter@web.de>
2024-01-17 14:26:53 +0100

    feat(Project-Manager): Enhance New Project Form Buttons Functionality
    - Added functionality to the "Save" button in the new project form.
    
      - When clicked, the input value is used to create a new project button.
    
      - The new button is appended underneath the "Create New Project" button.
    
    - Implemented functionality for the "Cancel" button.
    
      - When clicked, the form disappears, and the input is cleared.

--- src/projectManager.js
+++ src/projectManager.js
@@ -7,6 +7,8 @@ class ProjectManager {
     this.newProjectButton = DomElements.newProjectButton;
     this.projectNav = DomElements.projects;
     this.initializeEventListeners();
+    this.newProjectForm = false;
+    this.titleInput = null;
   }
   // event listener for the new project button
   initializeEventListeners() {
@@ -18,32 +20,75 @@ class ProjectManager {
 
   //function for the input form to create a new project
   addNewProject() {
-    const newForm = document.createElement("div");
-    newForm.className = "project-form";
+    if (this.newProjectForm) {
+      alert("Please fill out the new project form");
+    } else {
+      const newForm = document.createElement("div");
+      newForm.className = "project-form";
 
-    const titleInput = document.createElement("input");
-    titleInput.className = "project-title-input";
-    titleInput.placeholder = "Project Title";
+      this.titleInput = document.createElement("input");
+      this.titleInput.className = "project-title-input";
+      this.titleInput.placeholder = "Project Title";
 
-    const buttonContainer = document.createElement("div");
-    buttonContainer.className = "project-button-container";
+      const buttonContainer = document.createElement("div");
+      buttonContainer.className = "project-button-container";
 
-    const saveButton = document.createElement("button");
-    saveButton.className = "project-save-button";
-    saveButton.textContent = "Save";
+      const saveButton = document.createElement("button");
+      saveButton.className = "project-save-button";
+      saveButton.textContent = "Save";
+      saveButton.addEventListener(
+        "click",
+        this.createProjectNavLink.bind(this)
+      );
 
-    const cancelButton = document.createElement("button");
-    cancelButton.className = "project-cancel-button";
-    cancelButton.textContent = "Cancel";
+      const cancelButton = document.createElement("button");
+      cancelButton.className = "project-cancel-button";
+      cancelButton.textContent = "Cancel";
+      cancelButton.addEventListener("click", this.cancelNewProject.bind(this));
 
-    buttonContainer.append(saveButton, cancelButton);
-    newForm.append(titleInput, buttonContainer);
+      buttonContainer.append(saveButton, cancelButton);
+      newForm.append(this.titleInput, buttonContainer);
 
-    this.projectNav.appendChild(newForm);
+      this.projectNav.appendChild(newForm);
+      this.newProjectForm = true;
+    }
+  }
+
+  // function for adding a button with the project title to the nav bar
+  createProjectNavLink() {
+    if (this.titleInput.value !== "") {
+      //create new button with the value of the input
+      const newNavLink = document.createElement("button");
+      newNavLink.className = "new-project-nav-btn";
+      newNavLink.textContent = this.titleInput.value;
+      //add an icon before the title
+      const icon = document.createElement("img");
+      icon.className = "project-icon";
+      icon.src = "../src/media/target.png";
+      newNavLink.prepend(icon);
+
+      this.projectNav.appendChild(newNavLink);
+      // find the project form to remove it from the DOM
+      const projectForm = document.querySelector(".project-form");
+      this.projectNav.removeChild(projectForm);
+      // set back the value of the project form so a new one can pop up
+      this.newProjectForm = false;
+    } else {
+      alert("please fill out the title input");
+    }
+  }
+
+  // function for canceling the new project form
+  cancelNewProject() {
+    this.titleInput.value = "";
+    const projectForm = document.querySelector(".project-form");
+    this.projectNav.removeChild(projectForm);
+    this.newProjectForm = false;
   }
 }
 
 export default ProjectManager;
 
-// New project Button click:
-// Form poppt auf direkt unter dem button mit einem Input Feld und 2 buttons 1:Ad 2: Cancel
+// new project input form save click:
+// delete new project form coden
+// wenn man auf den button klickt öffnet sich eine neue view page mit dem dazugehörigen project title
